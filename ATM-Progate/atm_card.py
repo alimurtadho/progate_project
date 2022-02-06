fcfd8511883d8c344f320b3b887f94544cabaed5
murtadho <id.alimurtadho@gmail.com>
2022-02-06 21:20:01 +0700

    up

---
+++ ATM-Progate/atm_card.py
@@ -0,0 +1,11 @@
+class ATMcard:
+    # mengembalikan nilai dari
+    def __init__(self, defaultPin, defaultBalance):
+        self.defaultPin = defaultPin
+        self.defaultBalance = defaultBalance
+
+    def cekAwal(self):
+        return self.defaultPin
+
+    def cekSaldoAwal(self):
+        return self.defaultBalance
